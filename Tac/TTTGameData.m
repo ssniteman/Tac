cc972dbe624fcbbc8f05030380e893e56687f635
Shane Sniteman <ssniteman@gmail.com>
2014-08-29 09:22:07 -0400

    robot choose/find blocking spot/find winning spot

--- Tac/TTTGameData.m
+++ Tac/TTTGameData.m
@@ -45,15 +45,15 @@
         self.player1Turn = YES;
         
         possibilities = @[
-                         @[@0,@1,@2],
-                         @[@3,@4,@5],
-                         @[@6,@7,@8],
-                         @[@0,@3,@6],
-                         @[@1,@4,@7],
-                         @[@2,@5,@8],
-                         @[@0,@4,@8],
-                         @[@2,@4,@6]
-                         ];
+                          @[@0,@1,@2],
+                          @[@3,@4,@5],
+                          @[@6,@7,@8],
+                          @[@0,@3,@6],
+                          @[@1,@4,@7],
+                          @[@2,@5,@8],
+                          @[@0,@4,@8],
+                          @[@2,@4,@6]
+                          ];
         
     }
     return self;
@@ -77,8 +77,58 @@
     }
 }
 
+-(void)checkForWinner
+{
+    
+    BOOL winner = NO;
+    
+    for (NSArray * possibility in possibilities)
+        
+    {
+        TTTTouchSpot * spot0 = self.spots[[possibility[0]intValue]];
+        TTTTouchSpot * spot1 = self.spots[[possibility[1] intValue]];
+        TTTTouchSpot * spot2 = self.spots[[possibility[2] intValue]];
+        
+        if(spot0.player == spot1.player && spot1.player == spot2.player && spot0 != 0) {
+            winner = YES;
+        }
+        
+        int emptySpots = 0;
+        
+        for(TTTTouchSpot * spot in self.spots)
+        {
+            
+            if (spot.player==0) {
+                
+                emptySpots++;
+                
+            }
+            
+            if (emptySpots ==0 && !winner){
+                
+            }
+        }
+    }
+}
+
+
 - (void)robotChooseSpot
 {
+    if ([self findWinningSpot])
+    {
+        
+        self.player1Turn = !self.player1Turn;
+        [self checkForWinner];
+    }
+    
+    if ([self findBlockingSpot])
+    {
+        
+        self.player1Turn = !self.player1Turn;
+        [self checkForWinner];
+    }
+    
+    
     for (TTTTouchSpot * spot in self.spots)
     {
         if (spot.player == 0)
@@ -99,25 +149,20 @@
     for (NSArray * possibility in possibilities)
         
     {
-        TTTTouchSpot * spot0 = self.spots[[possibility[0]intValue]];
-        TTTTouchSpot * spot1 = self.spots[[possibility[1] intValue]];
-        TTTTouchSpot * spot2 = self.spots[[possibility[2] intValue]];
-        
-        if (spot0.player == 2 && spot1.player == 2 && spot2.player == 0)
+        if ([self checkForSpotWithSpots:possibility player:2])
         {
-            spot2.player = 2;
             return YES;
         }
         
-        if (spot0.player == 2 && spot2.player == 2 && spot1.player == 0)
+        NSArray * possibility2 = @[possibility[1],possibility[2],possibility[0]];
+        if ([self checkForSpotWithSpots:possibility2 player: 2])
         {
-            spot1.player = 2;
             return YES;
         }
         
-        if (spot2.player == 2 && spot1.player == 2 && spot0.player == 0)
+        NSArray * possibility3 = @[possibility[2],possibility[0],possibility[1]];
+        if ([self checkForSpotWithSpots:possibility3 player: 2])
         {
-            spot0.player = 2;
             return YES;
         }
     }
@@ -125,60 +170,57 @@
     return NO;
 }
 
+- (BOOL)checkForSpotWithSpots:(NSArray *)spots player:(int)player
+{
+    TTTTouchSpot * spot0 = self.spots[[spots[0]intValue]];
+    TTTTouchSpot * spot1 = self.spots[[spots[1] intValue]];
+    TTTTouchSpot * spot2 = self.spots[[spots[2] intValue]];
+    
+    if(spot0.player == 2 && spot1.player == 2 && spot0.player == 0)
+    {
+        return YES;
+    }
+    
+    return NO;
+}
+
+
 - (BOOL)findBlockingSpot
 {
     for (NSArray * possibility in possibilities)
         
     {
-        TTTTouchSpot * spot0 = self.spots[[possibility[0]intValue]];
-        TTTTouchSpot * spot1 = self.spots[[possibility[1] intValue]];
-        TTTTouchSpot * spot2 = self.spots[[possibility[2] intValue]];
-    }
-}
-
-- (BOOL)findRandomSpot
-{
-    for (NSArray * possibility in possibilities)
+        if ([self checkForSpotWithSpots:possibility player:1])
+        {
+            return YES;
+        }
         
-    {
-        TTTTouchSpot * spot0 = self.spots[[possibility[0]intValue]];
-        TTTTouchSpot * spot1 = self.spots[[possibility[1] intValue]];
-        TTTTouchSpot * spot2 = self.spots[[possibility[2] intValue]];
-    }
-}
-
--(void)checkForWinner
-{
-
-    BOOL winner = NO;
+        NSArray * possibility2 = @[possibility[1],possibility[2],possibility[0]];
+        if ([self checkForSpotWithSpots:possibility2 player: 1])
+        {
+            return YES;
+        }
+        
+        NSArray * possibility3 = @[possibility[2],possibility[0],possibility[1]];
+        if ([self checkForSpotWithSpots:possibility3 player: 1])
+        {
+            return YES;
+        }
+     }
     
-    for (NSArray * possibility in possibilities)
-        
-    {
-        TTTTouchSpot * spot0 = self.spots[[possibility[0]intValue]];
-        TTTTouchSpot * spot1 = self.spots[[possibility[1] intValue]];
-        TTTTouchSpot * spot2 = self.spots[[possibility[2] intValue]];
-        
-        if(spot0.player ==spot1.player && spot1.player == spot2.player && spot0 != 0) {
-            winner = YES;
-            }
-            
-        for(TTTTouchSpot * spot in self.spots)
-            {
-
-                if (spot.player==0) {
-                    
-                    emptySpots++;
-                            
-                    }
-                    
-                if (emptySpots ==0 && !winner){
-                        
-                }
-            }
-    }
+    return NO;
 }
 
-
-
-@end
+    //- (BOOL)findRandomSpot
+    //{
+    //    for (NSArray * possibility in possibilities)
+    //
+    //    {
+    //        TTTTouchSpot * spot0 = self.spots[[possibility[0]intValue]];
+    //        TTTTouchSpot * spot1 = self.spots[[possibility[1] intValue]];
+    //        TTTTouchSpot * spot2 = self.spots[[possibility[2] intValue]];
+    //    }
+    //}
+    //
+    
+    @end
